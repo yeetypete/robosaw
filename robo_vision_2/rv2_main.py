f9d759bdd9aae0f609e4855608318799407a38a2
Theodor Waalberg <twaal@bu.edu>
2022-03-10 17:42:49 -0500

    Update rv2_main.py

--- robo_vision_2/rv2_main.py
+++ robo_vision_2/rv2_main.py
@@ -20,15 +20,15 @@ def main():
             wood_loaded = rv.wood_is_loaded(model,caps[0])
 
             # Find the angle
-        #rv.find_angle_display(model,caps[1])
-        angle = rv.find_angle(model,caps[1])
-        print("Angle: " + str(angle))
+        rv.find_angle_display(model,caps[1])
+        #angle = rv.find_angle(model,caps[1])
+        #print("Angle: " + str(angle))
 
             # Find the distance of the line from the blade's plane of intersection
         #rv.display_center_cap(model,caps[2])
-        #rv.find_center_display(model,caps[2])
-        dist = rv.find_distance(model,caps[2])
-        print("Distance: " + str(dist))
+        rv.find_center_display(model,caps[2])
+        #dist = rv.find_distance(model,caps[2])
+        #print("Distance: " + str(dist))
 
             # Close the captures before terminating!
         for cap in caps:
