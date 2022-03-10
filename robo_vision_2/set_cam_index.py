dc2fb26666707437256ae6f981a36c72670f3576
Theodor Waalberg <twaal@bu.edu>
2022-03-10 18:02:16 -0500

    Update set_cam_index.py

--- robo_vision_2/set_cam_index.py
+++ robo_vision_2/set_cam_index.py
@@ -3,6 +3,26 @@ import numpy as np
 import time
 import sys
 
+################################################
+color_cam_id = 2
+angle_cam_id = 0
+center_cam_id = 4
+thearray = [color_cam_id, angle_cam_id, center_cam_id]
+
+print(thearray)
+key = input()
+if key == 27:
+    print("Exiting without saving...")
+    sys.exit()
+if key == 's':
+    thearray = [color_cam_id, angle_cam_id, center_cam_id] 
+    # Also save this array as .npy
+    np.save('__calibrate__/color_angle_center_cam_id_array',thearray)
+    print(str(thearray) + ": Saved")
+    sys.exit()
+################################################
+
+
 def display_by_index(cam_id):
     name = "Camera ID: " + str(cam_id)
     cap = cv2.VideoCapture(cam_id)
