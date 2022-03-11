c35791704a9659e74bffac7049938cd2e79438cf
Theodor Waalberg <twaal@bu.edu>
2022-03-10 19:19:26 -0500

    Update Model.py

--- robo_vision_2/Model.py
+++ robo_vision_2/Model.py
@@ -240,7 +240,7 @@ class Model(object):
 
     # Other
     line_detection_threshold = 40 #100
-    center_line_detection_threshold = int(circle_rad/3)
+    center_line_detection_threshold = int(circle_rad)
     color_thresh_wood_detection = 20
 
     pass
