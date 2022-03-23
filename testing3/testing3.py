3d5db6efa33d7f112f68b71909f61dd7d95e0983
Theodor Waalberg <twaal@bu.edu>
2022-03-23 12:47:00 -0400

    Update testing3.py
    extra error checking and cleaner main function

--- testing3/testing3.py
+++ testing3/testing3.py
@@ -25,6 +25,13 @@ def initialize():
     return model,caps
     
 
+def close_caps(caps):
+    """ Close the captures before terminating """
+    for cap in caps:
+        print("\n\nReleasing capture: " + str(cap))
+        cap.release()
+    robosaw.motors.forceStop()
+
 def eject(model,caps):
     """ Ejects the wood. Pushes all wood out and raises the blade to default height """
     print("Ejecting wood")
@@ -33,108 +40,111 @@ def eject(model,caps):
     robosaw.motors.setSpeeds(0, 0)
     return
 
-def run(model,caps):
+def run():
     """ Intake at idle speed until wood is detected, 
     find the angle of the line, 
     rotate blade to match angle, 
     move line under blade and center it, 
     wait for confirmation button,
     make cut, raise blade """
+    try:
+        model,caps = initialize()
+    except:
+        print("Unable to initialize model and caps")
 
-    _pi = robosaw.init_gpio()
-    args = robosaw.init_args()
-    motor3 = robosaw.Actuator(_pin_M3PWM, _pin_M3DIR, _pin_M3EN, _pin_M3FLT, _pi)
+    try:
+        _pi = robosaw.init_gpio()
+        args = robosaw.init_args()
+        motor3 = robosaw.Actuator(_pin_M3PWM, _pin_M3DIR, _pin_M3EN, _pin_M3FLT, _pi)
 
-    # Check if wood is loaded
-    while not rv.wood_is_loaded(model,caps[0]): # wait for the wood
-        robosaw.motors.setSpeeds(args.speed, args.speed) # idle
-    #caps[0].release()
+        # Check if wood is loaded
+        while not rv.wood_is_loaded(model,caps[0]): # wait for the wood
+            robosaw.motors.setSpeeds(args.speed, args.speed) # idle
+        #caps[0].release()
 
-    # Once wood is loaded accumlate angle samples
-    #rv.find_angle_display(model,caps[1])
-    angles = []
-    while len(angles) < model.num_angle_samples:
-        # Get angles as it moves and save to angle[] array
-        robosaw.motors.setSpeeds(args.speed, args.speed) # idle
-        angle = rv.find_angle(model,caps[1])
-        print("Angle: " + str(angle))
-        if angle is not None:
-            #blade_angle = angle
-            angles.append(angle)
-    # find most likely angle based on removing outliers and taking the mean
-    blade_angle = model.best_angle(angles)
-    caps[1].release()
+        # Once wood is loaded accumlate angle samples
+        #rv.find_angle_display(model,caps[1])
+        angles = []
+        while len(angles) < model.num_angle_samples:
+            # Get angles as it moves and save to angle[] array
+            robosaw.motors.setSpeeds(args.speed, args.speed) # idle
+            angle = rv.find_angle(model,caps[1])
+            print("Angle: " + str(angle))
+            if angle is not None:
+                #blade_angle = angle
+                angles.append(angle)
+        # find most likely angle based on removing outliers and taking the mean
+        blade_angle = model.best_angle(angles)
+        caps[1].release() # Close the angle camera
 
-    # Stop or slow the wood
-    # ... TODO ...
-    robosaw.motors.setSpeeds(args.speed - 50, args.speed - 50)
+        # Stop or slow the wood
+        # ... TODO ...
+        robosaw.motors.setSpeeds(args.speed - 50, args.speed - 50)
 
-    # Rotate the blade to correct angle
-    # ... TODO ...
-    print("\nRotate blade to " + str(blade_angle) + " degrees.\n")
+        # Rotate the blade to correct angle
+        # ... TODO ...
+        print("\nRotate blade to " + str(blade_angle) + " degrees.\n")
 
-    # Move the line close to the center and slow down
-    # ... TODO ...
-    #while not rv.wood_is_under(model,caps[0]):
-        #print("...")
-    # Stop the wood under the blade
-    #rv.find_center_display(model,caps[2])
-    while True:
-        dist = rv.find_distance(model,caps[2])
-        if (dist is not None):
-            if (dist < 0):
+        # Move the line close to the center and slow down
+        # ... TODO ...
+        #while not rv.wood_is_under(model,caps[0]):
+            #print("...")
+        # Stop the wood under the blade
+        #rv.find_center_display(model,caps[2])
+        while True:
+            dist = rv.find_distance(model,caps[2])
+            if (dist is not None):
+                if (dist < 0):
+                    print("Distance: " + str(dist))
+                    robosaw.motors.setSpeeds(0,0)
+                    break
+                robosaw.feed(abs(dist), int(args.speed - 50))
                 print("Distance: " + str(dist))
-                robosaw.motors.setSpeeds(0,0)
-                break
-            robosaw.feed(abs(dist), int(args.speed - 50))
-            print("Distance: " + str(dist))
 
-    # Calculate overshoot from stop point
-    time.sleep(1) # wait a second to see if the wood oversoots
-    dist = rv.find_distance(model,caps[2])
-    print("\nOvershoot/undershoot distance: " + str(-dist))
+        # Calculate overshoot from stop point
+        time.sleep(1) # wait a second to see if the wood oversoots
+        dist = rv.find_distance(model,caps[2])
+        print("\nOvershoot/undershoot distance: " + str(-dist))
 
-    # Spin the blade
-    # ... TODO ...
-    _pi.write(blade_relay_pin, 1)
+        # Spin the blade
+        # ... TODO ...
+        _pi.write(blade_relay_pin, 1)
 
-    # Lower the blade as it spins
-    # ... TODO ...
+        # Lower the blade as it spins
+        # ... TODO ...
 
-    motor3.setSpeed(480)
-    time.sleep(1)
+        motor3.setSpeed(480)
+        time.sleep(1)
 
-    # Raise blade again
-    # ... TODO ...
-    motor3.setSpeed(-480)
-    time.sleep(1)
-    motor3.setSpeed(0)
+        # Raise blade again
+        # ... TODO ...
+        motor3.setSpeed(-480)
+        time.sleep(1)
+        motor3.setSpeed(0)
 
-    # Stop the blade
-    # ... TODO ...
+        # Stop the blade
+        # ... TODO ...
 
-    _pi.write(blade_relay_pin, 0)
+        _pi.write(blade_relay_pin, 0)
 
-    # Eject the wood
-    # ... TODO ...
+        # Eject the wood
+        # ... TODO ...
 
-    #robosaw.motors.motor2.setSpeed(args.speed)
-    #time.sleep(2)
+        #robosaw.motors.motor2.setSpeed(args.speed)
+        #time.sleep(2)
 
-def close_caps(caps):
-    """ Close the captures before terminating """
-    for cap in caps:
-        print("\n\nReleasing capture: " + str(cap))
-        cap.release()
+        close_caps() # Always close captures after
+    except:
+        close_caps() # Close caps if program fails or gets cancelled
 
-    robosaw.motors.forceStop()
+
 
 
 if __name__ == "__main__":
     s = time.perf_counter()
-    model,caps = initialize()
     #eject(model,caps) # Runs the feeding mechanism as long as 'feed' button is pressed
-    run(model,caps) # Once 'Run' button pressed, waits for wood, finds angle, centers under the blade and makes the cut
-    close_caps(caps) # Close cameras before shutting off
+
+    run() # Once 'Run' button pressed, waits for wood, finds angle, centers under the blade and makes the cut
+
     elapsed = time.perf_counter() - s
     print(f"{__file__} executed in {elapsed:0.2f} seconds.")
